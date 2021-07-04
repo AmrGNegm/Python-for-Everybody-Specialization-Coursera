124598c144a20c04df93c00d719ce0a3b12384b1
Amr Negm <amr.g.negm@gmail.com>
2021-07-04 13:59:45 +0200

    Create Quiz.py

---
+++ 1-  Programming for Everybody (Getting Started with Python)/Quiz.py
@@ -0,0 +1,15 @@
+# the code below almost works
+print("hello world")
+
+# The code below almost works
+
+name = input("Enter your name")
+print("Hello" + ' ' + name)
+
+# This first line is provided for you
+
+hrs = float(input("Enter Hours:"))
+rate = float(input("Enter Rate:"))
+pay = hrs * rate
+print("Pay:" + ' ' + str(pay)) 
+
